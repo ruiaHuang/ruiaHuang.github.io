f318ea188820b1d07e66ee4c0b82902cf0d34512
Ray <735321845@qq.com>
2024-06-18 22:41:10 +0800

    更新文案

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Telegram mini app</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        试试看
-      </p>
       {/* 在此处添加带有警告回调的按钮 */}
       <div className="card">
         <button
