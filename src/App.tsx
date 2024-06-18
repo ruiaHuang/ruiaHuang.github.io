da18fb264eda5199a96c3e8c0bb27175e930fece
Ray <735321845@qq.com>
2024-06-18 22:27:56 +0800

    修改文案

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Telegram mini app</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +28,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        试试看
       </p>
       {/* 在此处添加带有警告回调的按钮 */}
       <div className="card">
