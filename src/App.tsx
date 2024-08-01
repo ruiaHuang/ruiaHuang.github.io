4980ac1e7334b208962b745d1f885ad9700df8a0
Ray <735321845@qq.com>
2024-08-01 22:30:09 +0800

    feat: 使用tg

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css';
+
+import WebApp from '@twa-dev/sdk';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const hash = window.location.hash.slice(1);
+  console.log(hash); // tgWebAppData=...&tgWebAppVersion=6.2&...
+
+  const params = new URLSearchParams(hash);
+  console.log(params.get('tgWebAppVersion')); // "6.2"
+
+  return (
+    <>
+      <h1>Telegram mini app</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      {/* 在此处添加带有警告回调的按钮 */}
+      <div className="card">
+        <button
+          onClick={() =>
+            WebApp.showAlert(`Hello World! Current count is ${count}`)
+          }
+        >
+          显示警告
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import WebApp from '@twa-dev/sdk';
+import eruda from 'eruda';
+
+WebApp.ready();
+eruda.init();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
